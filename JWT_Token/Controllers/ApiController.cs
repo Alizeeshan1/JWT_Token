4dbfb24e0daf0a3910d0a0d26959dcd8148ecf23
Zeeshan Ali <Zeeshan Ali@DESKTOP-8U6BEFO>
2022-11-29 23:43:49 -0800

    Add project files.

---
+++ JWT_Token/Controllers/ApiController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace JWT_Token.Controllers
+{
+    
+        [ApiController]
+        [Route("[controller]")]
+        public class ApiController : ControllerBase
+        {
+            [Authorize]
+            [HttpGet("getWithAny")]
+            public IActionResult GetWithAny()
+            {
+                return Ok(new { Message = $"Hello World {GetUsername()}" });
+            }
+
+            [Authorize(AuthenticationSchemes = "SecondJwtScheme")]
+            //[Authorize(Policy = "OnlySecondJwtScheme")]
+            [HttpGet("getWithSecondJwt")]
+            public IActionResult GetWithSecondJwt()
+            {
+                return Ok(new { Message = $"Hello World {GetUsername()}" });
+            }
+
+            private string? GetUsername()
+            {
+                return HttpContext.User.Claims
+                    .Where(x => x.Type == ClaimTypes.Name)
+                    .Select(x => x.Value)
+                    .FirstOrDefault();
+            }
+        }
+}
